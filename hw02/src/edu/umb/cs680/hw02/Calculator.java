3b0836b8fc0fc89687ea28eb673f04f3465e2125
allenwu9014 <allenwu9014@gmail.com>
2020-04-10 19:04:56 -0400

    hw2-2 version 1

---
+++ hw02/src/edu/umb/cs680/hw02/Calculator.java
@@ -0,0 +1,14 @@
+package edu.umb.cs680.hw02;
+
+public class Calculator {
+    public float multiply(float x, float y) {
+        return x * y;
+    }
+
+    public float divide(float x, float y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("division by zero");
+        }
+        return x / y;
+    }
+}
