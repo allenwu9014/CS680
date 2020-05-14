3f4130217afdc56e95188997322948652c9bcb0b
allenwu9014 <allenwu9014@gmail.com>
2020-05-14 19:07:10 -0400

    hw06 version4 with two versions, singleton and state. ant build file are hw06-singleton.xml and hw06-state.xml in corresponding directories.

--- hw6/State/src/edu/umb/cs680/hw06/State.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw06;
-
- public interface State {
-     DVDPlayer player = new DVDPlayer();
-   public void openCloseButtonPushed();
-   public void playButtonPushed();
-    public void stopButtonPushed();
-
-}
