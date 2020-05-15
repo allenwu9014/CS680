966c1825d909fac097ae154fdfda4b77aa4e55d2
allenwu9014 <allenwu9014@gmail.com>
2020-05-14 20:34:27 -0400

    hw08 version1

---
+++ hw8/test/src/edu/umb/cs680/hw08/FileSystemTest.java
@@ -0,0 +1,35 @@
+package edu.umb.cs680.hw08;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+public class FileSystemTest {
+
+    private LocalDateTime localdatetime1 = LocalDateTime.now();
+
+
+    private Directory[] LinkedListToArray(LinkedList<Directory> rootDirs) {
+        Directory[] roots = new Directory[rootDirs.size()];
+        int i = 0;
+        for (Directory root : rootDirs) {
+            roots[i++] = root;
+        }
+        return roots;
+    }
+
+    @Test
+    public void addgetRootDirectotyTest() {
+        Directory root = new Directory(null, "Root", 0, localdatetime1);
+        FileSystem filesystem = FileSystem.getFileSystem();
+        filesystem.addRootDir(root);
+        LinkedList<Directory> roots = filesystem.getRootDirs();
+        Directory[] actual = LinkedListToArray(roots);
+        Directory[] expected = {root};
+        assertArrayEquals(actual, expected);
+    }
+
+
+
+
+}
